547a3ed8e3c92879a72d44a7df34d10f2c0a4cd3
Shiyam4896 <133642510+Shiyam4896@users.noreply.github.com>
2023-09-27 11:20:02 +0530

    Update Longest_SubString.js
    Visual changes in appearance of the code.

--- Longest_SubString.js
+++ Longest_SubString.js
@@ -1,8 +1,5 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLongestSubstring = function (s) {
+var lengthOfLongestSubstring = function (s) 
+{
     let set = new Set();
     let left = 0;
     let maxSize = 0;
@@ -10,9 +7,11 @@ var lengthOfLongestSubstring = function (s) {
     if (s.length === 0) return 0;
     if (s.length === 1) return 1;
 
-    for (let i = 0; i < s.length; i++) {
+    for (let i = 0; i < s.length; i++) 
+    {
 
-        while (set.has(s[i])) {
+        while (set.has(s[i])) 
+        {
             set.delete(s[left])
             left++;
         }
